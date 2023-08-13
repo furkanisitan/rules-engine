b8509e3da8b25e23922edc99cb4e58a50bea2c58
furkanisitan <furkanisitan96@gmail.com>
2023-08-13 16:56:05 +0300

    Add Options property to IRule interface

--- src/RuleDesignPattern.RuleEngine/Rules/IRule.cs
+++ src/RuleDesignPattern.RuleEngine/Rules/IRule.cs
@@ -4,6 +4,9 @@ public interface IRule<in TRequest, TResponse>
     where TRequest : IRuleRequest
     where TResponse : IRuleResponse, new()
 {
+    RuleOptionAttribute? Options =>
+        Attribute.GetCustomAttribute(GetType(), typeof(RuleOptionAttribute)) as RuleOptionAttribute;
+
     bool CanApply(TRequest request);
 
     TResponse Apply(TRequest request, TResponse response);
--- src/RuleDesignPattern.RuleEngine/Rules/IUnlinkedRule.cs
+++ src/RuleDesignPattern.RuleEngine/Rules/IUnlinkedRule.cs
@@ -1,6 +1,6 @@
 ﻿namespace RuleDesignPattern.RuleEngine.Rules;
 
-public interface IUnlinkedRule<in TRequest, TResponse> : IRule<TRequest, TResponse>
+public interface IUnLinkedRule<in TRequest, TResponse> : IRule<TRequest, TResponse>
     where TRequest : IRuleRequest
     where TResponse : IRuleResponse, new()
 {
