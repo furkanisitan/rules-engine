7649fdd2aac1955682046d6300b50942d1c23e94
furkanisitan <furkanisitan96@gmail.com>
2023-08-13 21:10:47 +0300

    Add overload Execute methods to RuleExecutor

--- src/RuleDesignPattern.RuleEngine/RuleExecutor.cs
+++ src/RuleDesignPattern.RuleEngine/RuleExecutor.cs
@@ -45,4 +45,49 @@ public static class RuleExecutor
             .OrderByDescending(r => r.RuleOption?.RuleType ?? RuleType.None)
             .ToList();
     }
+
+    /// <summary>
+    ///     Executes the rule of type <typeparamref name="TRule" /> and other rules that linked on this type.
+    /// </summary>
+    /// <inheritdoc cref="Execute{TRequest,TResponse}" />
+    public static TResponse Execute<TRule, TRequest, TResponse>(TRequest request, TResponse response)
+        where TRule : IRule<TRequest, TResponse>, new()
+        where TRequest : IRuleRequest
+        where TResponse : IRuleResponse, new()
+    {
+        return Execute(typeof(TRule), request, response);
+    }
+
+    /// <summary>
+    ///     Executes the rule of type <paramref name="ruleType" /> and other rules that linked on this type.
+    /// </summary>
+    /// <remarks>
+    ///     Rules are executed recursively.
+    ///     If the linked rule also has a linked rule, that rule will also be executed.
+    /// </remarks>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <param name="ruleType">The type of rule to execute.</param>
+    /// <param name="request">The input parameter of the rule.</param>
+    /// <param name="response">The result parameter of the rule.</param>
+    /// <returns>The <paramref name="response" /> object.</returns>
+    /// <exception cref="ArgumentNullException">If one of the arguments is null.</exception>
+    private static TResponse Execute<TRequest, TResponse>(Type ruleType, TRequest request, TResponse response)
+        where TRequest : IRuleRequest
+        where TResponse : IRuleResponse, new()
+    {
+        ArgumentNullException.ThrowIfNull(ruleType);
+        ArgumentNullException.ThrowIfNull(request);
+        ArgumentNullException.ThrowIfNull(response);
+
+        if (Activator.CreateInstance(ruleType) is not IRule<TRequest, TResponse> rule) return response;
+
+        if (!rule.CanApply(request, response)) return response;
+        response = rule.Apply(request, response);
+
+        return rule.Options is null
+            ? response
+            : rule.Options.LinkedRules.Aggregate(response,
+                (current, linkedRule) => Execute(linkedRule, request, current));
+    }
 }
